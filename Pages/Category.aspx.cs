10e24813d9f79e58f748ced56b7ad16279bcc0a4
ThaoPham <phamthithao1711@gmail.com>
2019-11-28 23:47:49 +0700

    update code

--- Pages/Category.aspx.cs
+++ Pages/Category.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Web.Configuration;
 
 namespace MITComputerWebForm.Pages
 {
@@ -11,6 +13,18 @@ namespace MITComputerWebForm.Pages
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            //Khai báo mở kết nối đến CSDL
+            using (SqlConnection connection = new SqlConnection(share.Share.connectionString))
+            {
+                string querystring = "SELECT * FROM DANHMUC";
+                SqlCommand command = new SqlCommand(querystring,connection);
+                connection.Open();
+                //Khai báo đối tượng đọc dữ liệu từ kết quả câu truy vấn 
+                SqlDataReader datareader = command.ExecuteReader();
+                //Đọc dữ liệu từ đối tượng DataReader
+
+
+            }
 
         }
     }
